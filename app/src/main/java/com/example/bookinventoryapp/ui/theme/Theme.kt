bd1ee0979829dc277c40e0ac937e5478256ef53a
Jonathan <jonathan.carr031@gmail.com>
2025-02-03 00:51:32 -0500

    Initial Commit

--- app/src/main/java/com/example/bookinventoryapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/bookinventoryapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.bookinventoryapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
