a15c1d984e33e65b9c0ccbd7e669987bd1010879
Jonathan <jonathan.carr031@gmail.com>
2025-02-15 23:40:48 -0500

    Updating Issue with setting value

--- app/src/main/java/com/example/bookinventoryapp/book/presentation/book_details/BookDetailsState.kt
+++ app/src/main/java/com/example/bookinventoryapp/book/presentation/book_details/BookDetailsState.kt
@@ -3,7 +3,7 @@ package com.example.bookinventoryapp.book.presentation.book_details
 import com.example.bookinventoryapp.book.domain.Book
 
 data class BookDetailsState(
-    val bookIsbn: String = "",
+    var bookIsbn: String = "",
     val selectedBook: Book? = null,
     val isEditing: Boolean = false,
     val isLoading: Boolean = false,
